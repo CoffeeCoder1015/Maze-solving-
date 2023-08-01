7f816cb15ae9428579321125c863d5d8c2312f3a
CoffeeCoder1015 <heroicnathan@gmail.com>
2023-08-01 23:28:58 +1200

    Basic graphics functions

---
+++ app/main.js
@@ -0,0 +1,11 @@
+function preload() { //load images etc
+    /* spriteSheet = loadImage("sprites.png"); */
+}
+
+function setup() {//sets up canvas size and other desired content
+    createCanvas(800, 800);
+}
+
+function draw(){//draw loop
+
+}
