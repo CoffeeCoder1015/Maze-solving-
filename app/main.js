939b61d22064f1b593c949a1a2bf776afefd1442
CoffeeCoder1015 <heroicnathan@gmail.com>
2023-08-02 15:47:27 +1200

    Setup functions and character

--- app/main.js
+++ app/main.js
@@ -1,3 +1,16 @@
+let board = Array(20).fill().map(() => Array(20).fill(0));
+
+class Bob{ //character
+    constructor(x,y,target_x,target_y){
+        this.x = x
+        this.y = y
+        this.target_x = target_x
+        this.target_y = target_y
+    }
+
+    
+}
+
 function preload() { //load images etc
     /* spriteSheet = loadImage("sprites.png"); */
 }
@@ -6,6 +19,11 @@ function setup() {//sets up canvas size and other desired content
     createCanvas(800, 800);
 }
 
+function generateGrid(){
+
+}
+
 function draw(){//draw loop
 
 }
+
